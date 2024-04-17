0b874291aad8b7d80838eb3cfdf89ccc2fd52ea3
LGpicinin <112085670+LGpicinin@users.noreply.github.com>
2024-04-17 17:38:00 -0300

    correct somethings

--- Trabalho-1-UDP/client.py
+++ Trabalho-1-UDP/client.py
@@ -2,10 +2,12 @@ import socket
 import sys
 from operator import itemgetter
 
-TEST_FILE = "./clientFiles/outroTeste.txt"
+TEST_FILE = "./clientFiles/comparando.txt"
 
 port = 5000
 host = "localhost"
+
+TAM_SOCKET = 10050
 
 
 def createSocket():
@@ -18,76 +20,88 @@ def createSocket():
         sys.exit()
 
 def sendMessagesToServer(s):
- 
+        # pede ao usuário o nome do arquivo desejado
         text = input('\nEscreva o nome do arquivo que deseja acessar: ')
         msg = "GET " + text
         msg = msg.encode()
 
+        # pede ao usuário se ele quer simular a perda de arquivo
         lost = input('\nDeseja simular a perda de dado(digite sim ou não)?')
 
 
         try:
+            # envia requisição
             s.sendto(msg, (host, port))
             file = open(TEST_FILE, "a")
             rightSockets = []
             wrongSockets = []
 
             j = 0
+            # começa as respostas do servidor
             while (1):
-                receive = s.recv(1050)
+                receive = s.recv(TAM_SOCKET)
                 receive = receive.decode()
-
+                
+                # caso o servidor tenha terminado de enviar os pacotes, finaliza
                 if receive == "game over":
                     break
+                # caso a requisição do usuário esteja errada, finaliza
                 elif receive == "Formato de requisição não aceito" or receive == "Arquivo não encontrado":
                     print(receive)
                     sys.exit()
 
+                # separa a resposta recebida em tamanho - numeração - dados
                 res = receive.split("-///-")
-
                 lenghtData = res[0]
                 socketNumber = res[1]
                 data = res[2]
-
+                
+                # caso queira simular a perda de pacotes, os dados recebidos são zerados
                 if lost=="sim" and (j==0 or j==1):
                     data = ""
-                j = j+1
+                    j = j+1
 
+                # caso o tamanho dos dados recebidos não bata com tamanho que o servidor 
+                # mandou, é guardado a numeração em uma lista de pacotes errados
                 if len(data) != int(lenghtData):
                     wrongSockets.append(socketNumber)
+                # caso os tamanhos batam, é guardado em uma lista de pacotes certos
                 else:
                     rightSockets.append([int(socketNumber), data])
                 
 
+            # caso tenha pacotes errados, pede eles novamente
             if len(wrongSockets) > 0:
+                # primeiro, é enviado as numerações dos pacotes errados para o servidor
                 msg = wrongSockets[0]
                 for i in range(1, len(wrongSockets)):
                     msg = msg + "," + wrongSockets[i]
                 msg = msg.encode()
                 s.sendto(msg, (host, port))
+
+                # depois, é recebido os pacotes correspondentes
                 for i in range(0, len(wrongSockets)):
-                    receive = s.recv(1024)
+                    receive = s.recv(TAM_SOCKET-50)
                     receive = receive.decode()
                     rightSockets.append([int(wrongSockets[i]), receive])
 
+                #reordena os pacotes para serem adicionados ao arquivo
                 rightSockets.sort(key=itemgetter(0))
 
+
+            #caso contrário, manda um OK
             else:
                 msg = "OK"
                 msg = msg.encode()
                 s.sendto(msg, (host, port))
 
+            #escreve os dados no arquivo
             for i in range(0, len(rightSockets)):
                 file.write(rightSockets[i][1])
 
 
-                    
-
-
             file.close()
             
-            #print('\nServer reply: ')
-            #print(data)
 
         except socket.error:
             print(socket.error)
--- Trabalho-1-UDP/server.py
+++ Trabalho-1-UDP/server.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import math
 import os
+from time import sleep
 
 TEST_FILE = './serverFiles/teste.txt'
 NAMES_FILE = './serverFiles/names.txt'
@@ -9,6 +10,8 @@ PATH = './serverFiles/'
 
 port = 5000
 host = "localhost"
+
+TAM_SOCKET = 10000
 
 
 def createSocket():
@@ -33,6 +36,7 @@ def bindSocket(s):
 
 def runServer(s):
     while 1:
+        # recebe requisição do cliente
         data, adress = s.recvfrom(1024)
         print("Nova requisição de cliente")
         
@@ -42,6 +46,7 @@ def runServer(s):
         data = data.decode()
         data = data.split(" ")
 
+        # verifica se o formato é válido
         if data[0] != "GET":
             msg = "Formato de requisição não aceito"
             msg = msg.encode()
@@ -49,41 +54,52 @@ def runServer(s):
             continue
 
         fileToOpen = ""
-
         fileToOpen = PATH + data[1]
 
+
+        # verifica se arquivo o requisitado existe
         if not os.path.exists(fileToOpen):
             msg = "Arquivo não encontrado"
             msg = msg.encode()
             s.sendto(msg, adress)
             continue
 
+        # abre o arquivo requisitado
         file = open(fileToOpen, "rt")
         i = 1
-        dataToSend = file.read(1024)
+        dataToSend = file.read(TAM_SOCKET)
 
+
+        # começa a enviar o arquivo
         print("Enviando sockets")
         while dataToSend:
             lenght = str(len(dataToSend))
             socketNumber = str(i)
 
+            # envia no formato: tamanho - numeração - dados
             reply = lenght + "-///-" + socketNumber + "-///-" + dataToSend
             reply = reply.encode()
 
             s.sendto(reply, adress)
-            dataToSend = file.read(1024)
+            dataToSend = file.read(TAM_SOCKET)
             i = i + 1
-                
+        
+        # envia uma mensagem para avisar que acabou de enviar
+        sleep(1)
         finalMessage = 'game over'
         s.sendto(finalMessage.encode(), adress)
 
         
+
+        # recebe uma mensagem de verificação do cliente para saber se tudo está certo
         data, adress = s.recvfrom(1024)
         data = data.decode()
-
+        # se está certo, recebe um OK
         if data == "OK":
             print("Envio realizado com sucesso \n\n")
-          
+        
+        # se não, recebe uma lista de números que representam os sockets
+        # que não chegaram ao cliente corretamente
         else:
             print("\nReenvio de arquivos necessário")
             file = open(fileToOpen, "rt")
@@ -95,13 +111,13 @@ def runServer(s):
             dataLenght = len(data)
             j = 0
             i = 1
-            reply = file.read(1024)
+            reply = file.read(TAM_SOCKET)
             while j<dataLenght:
                 if int(data[j]) == i:
-                    print(reply)
+                    #print(reply)
                     s.sendto(reply.encode(), adress)
                     j = j + 1
-                reply = file.read(1024)
+                reply = file.read(TAM_SOCKET)
                 i = i + 1
             print("Finalizando reenvio de arquivos \n\n")
 
