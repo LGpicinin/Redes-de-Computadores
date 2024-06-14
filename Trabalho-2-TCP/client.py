acf58442c0549e47feb03ac5eced5b45d091883c
LGpicinin <lgpicinin@gmail.com>
2024-06-14 10:28:46 -0300

    funcionando TCP

--- Trabalho-2-TCP/client.py
+++ Trabalho-2-TCP/client.py
@@ -1,13 +1,14 @@
 import socket
 import sys
-from operator import itemgetter
+from hashlib import md5
+import os
 
-TEST_FILE = "./clientFiles/comparando.txt"
+PATH = "./clientFiles/"
 
 port = 5000
 host = "localhost"
 
-
+# cria socket
 def createSocket():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,34 +18,132 @@ def createSocket():
         print('\nFailed to create socket. Error: ' + socket.error)
         sys.exit()
 
+# Arquivo - roda até cliente receber todo o arquivo
+def optionArq(socket):
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # digita nome do arquivo e envia
+    print("Digite o nome do arquivo desejado:")
+    nomeArq = input()
+    message = "Arquivo " + nomeArq
+    socket.send(message.encode())
+
+    # recebe retorno do servidor
+    res = socket.recv(1024)
+    res = res.decode()
+
+    if res == "NOT FOUND":
+        return
+    
+    # caso de sucesso - divide resposta em número de pacotes e hash do arquivo
+    resList = res.split(" ")
+    numberPackets = int(resList[1])
+    checkServer = resList[0]
+    i = 0
+    
+    # abre arquivo e inicia hash do cliente
+    file = open(PATH + nomeArq , "wb")
+    checkClient = md5()
+
+
+    while i<numberPackets:
+
+        # recebe resposta e divide em hash e dados
+        res = socket.recv(1024)
+        hashServer = res[0:16]
+        data = res[17:]
+
+        # verifica se as hash são iguais e pede de novo até dar certo
+        while md5(data).digest() != hashServer:
+            message = "NOK"
+            socket.send(message.encode())
+            res = socket.recv(1024)
+            hashServer = res[0:16]
+            data = res[17:]
+        
+        # atualiza hash do cliente
+        checkClient.update(data)
+        
+        # manda retorno positivo e escreve data no arquivo
+        message = "OK"
+        socket.send(message.encode())
+        file.write(data)
+        i = i + 1
+
+    # verifica se hash cliente = hash server
+    if checkClient.hexdigest() != checkServer:
+        print("O arquivo não é confiável")
+
+    print("\nAperte a tecla ENTER para continuar")
+
+    input()
+
+    file.close()
+
+
+# Chat - Roda até cliente digitar ADEUS
+def optionChat(socket):
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print("Iniciando conversa com o servidor. Para encerrar, digite ADEUS:\n\n")
+
+    print("Você: ")
+    message = input()
+
+    while message != "ADEUS":
+
+        socket.send(message.encode())
+
+        message = socket.recv(1024)
+
+        print("\nChat:\n" + message.decode())
+        
+        print("\nVocê: ")
+        message = input()
+
+    socket.send(message.encode())
+
 
 def Main():
 
-    s = createSocket()
-    s.connect((host,port))
+    # cria socket e anexa a uma porta
+    socket = createSocket()
+    socket.connect((host,port))
  
-    # message you send to server
-    message = "shaurya says geeksforgeeks"
+    # enaquanto não for selecionada a opção de Sair
     while True:
- 
-        # message sent to server
-        s.send(message.encode())
- 
-        # message received from server
-        data = s.recv(1024)
- 
-        # print the received message
-        # here it would be a reverse of sent message
-        print('Received from the server :',str(data.decode()))
- 
-        # ask the client whether he wants to continue
-        ans = input('\nDo you want to continue(y/n) :')
-        if ans == 'y':
-            continue
-        else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        message = ""
+
+        print("Selecione uma ação:\n1 - Sair\n2 - Receber arquivo\n3 - Chat")
+
+        option = input()
+        option = int(option)
+
+        # Sair
+        if option == 1:
+            message = "Sair"
+            socket.send(message.encode())
+            socket.close()
             break
-    # close the connection
-    s.close()
+        
+        # Arquivo
+        elif option == 2:
+            optionArq(socket)
+
+        # Chat
+        elif option == 3:
+            message = "Chat"
+            socket.send(message.encode())
+            optionChat(socket)
+        
+        else:
+            print("Opção inválida")
+            continue
+ 
+
  
 if __name__ == '__main__':
     Main()
--- Trabalho-2-TCP/server.py
+++ Trabalho-2-TCP/server.py
@@ -1,8 +1,8 @@
 import socket
 import sys
-import math
 import os
-from time import sleep
+import time
+from hashlib import md5
 from threading import Thread
  
 
@@ -10,10 +10,11 @@ TEST_FILE = './serverFiles/teste.txt'
 NAMES_FILE = './serverFiles/names.txt'
 PATH = './serverFiles/'
 
-port = 5000
-host = "localhost"
+PORT = 5000
+HOST = "localhost"
 
 
+# cria socket
 def createSocket():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,59 +24,158 @@ def createSocket():
         print('\nFailed to create socket. Error: ' + socket.error)
         sys.exit()
 
-
-
+# anexa socket a uma porta
 def bindSocket(s):
     try:
-        s.bind((host, port))
+        s.bind((HOST, PORT))
         print('\nBind socket')
     except socket.error:
         print('\nFailed to bind socket. Error: ' + socket.error)
         sys.exit()
 
 
- 
-# thread function
-def threaded(clientSocket, addr):
-    while True:
- 
-        # data received from client
-        data = clientSocket.recv(1024)
+def getCheckSumAndLength(file):
+
+    # começa a ler o arquivo e inicia hash
+    data = file.read(1000)
+    check = md5()
+    length = 0
+
+    # enquanto tiver dados no arquivo para ler
+    while data:
+        # atualiza hash e aumenta número de pacotes
+        check.update(data)
+        length = length + 1
+        data = file.read(1000)
+
+    # retorna a hash e o número de pacotes
+    return check.hexdigest(), length
+
+
+def optionArq(socket, dataList):
+
+    fileName = dataList[1]
+
+    fileToOpen = PATH + fileName
+
+
+    # verifica se arquivo o requisitado existe
+    if not os.path.exists(fileToOpen):
+        msg = "NOT FOUND"
+        msg = msg.encode()
+        socket.send(msg)
+        return
+    
+    # abre arquivo
+    file = open(fileToOpen, "rb")
+
+    # calcula hash de todo o arquivo e também o número de pacotes que serã enviados
+    check, numberPackets = getCheckSumAndLength(file)
+
+    # envia as informações calculadas
+    dataToSend = check + " " + str(numberPackets)
+    socket.send(dataToSend.encode())
+
+    time.sleep(0.1)
+
+    # abre o arquivo novamente e começa a ler
+    file = open(fileToOpen, "rb")
+    data = file.read(1000)
+
+    # enquanto tiver dados no arquivo para ler
+    while data:
+        
+        # calcula hash da parte que será enviada
+        hashServer = md5(data).digest()
+
+        # monta mensagem
+        dataToSend = b" ".join([hashServer, data])
+
+        # envia mensagem e recebe o retorno do cliente
+        socket.send(dataToSend)
+        res = socket.recv(1024)
+
+        # se a hash tiver falhado, envia de novo até dar certo
+        while res.decode() == "NOK":
+            hashServer = md5(data).digest()
+            dataToSend = b" ".join([hashServer, data])
+            socket.send(dataToSend)
+            res = socket.recv(1024)
+
+        # continua lendo o arquivo
+        data = file.read(1000)
 
         if not data:
-            print('Bye')
+            time.sleep(0.1)
+
+
+
+
+def optionChat(socket, threadNumber):
+
+    threadNumber = str(threadNumber)
+
+    # recebe mensagem e decodifica
+    message = socket.recv(1024)
+    message = message.decode()
+
+    # enquanto mensagem não for ADEUS 
+    while message!="ADEUS":
+
+        # imprime mensagem e manda a do server
+        print("\nUsuário " + threadNumber + ":\n" + message)
+        print("\nServer respondendo " + threadNumber + ": ")
+        message = input()
+        socket.send(message.encode())
+
+        # recebe mensagem e decodifica
+        message = socket.recv(1024)
+        message = message.decode()
+
+
+ 
+# função principal que roda na thread
+def threaded(clientSocket, threadNumber):
+    while True:
+ 
+        # recebe mensagem e decodifica
+        data = clientSocket.recv(1024)
+        data = data.decode()
+        dataList = data.split(" ")
+
+        if dataList[0] == "Sair":
             clientSocket.close()
-            # lock released on exit
             break
 
-        print("Receive data")
-        print(data.decode())
+        elif dataList[0] == "Arquivo":
+            optionArq(clientSocket, dataList)
+
+        elif dataList[0] == "Chat":
+            optionChat(clientSocket, threadNumber)
+
+
  
-        # reverse the given string from client
-        data = data[::-1]
- 
-        # send back reversed string to client
-        clientSocket.send(data)
- 
-    # connection closed
-    clientSocket.close()
  
  
 def Main():
 
+    # cria socket e começa a ouvir porta
     serverSocket = createSocket()
     bindSocket(serverSocket)
  
     serverSocket.listen()
     print("socket is listening")
- 
+    i = 0
+
     while True:
- 
-        # establish connection with client
+            
+        # aceita requisição do cliente
         clientSocket, addr = serverSocket.accept()
 
-        thread = Thread(target=threaded, args=(clientSocket, addr), daemon=True)
+        # cria thread pro cliente e inicia função principal
+        thread = Thread(target=threaded, args=(clientSocket, i), daemon=True)
         thread.start()
+        i = i+1
  
  
 if __name__ == '__main__':
